169531c855764e66de0b80bce6b5da5f53cf2fe9
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-10-26 14:47:42 -0600

    making great changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,7 @@ dependencies {
     implementation(libs.tensorflow.lite.support)
     // Object detection and tracking feature with custom bundled model
     implementation(libs.objects.detection.custom)
+//    implementation "androidx.compose.material3:material3:1.2.0"
+//    implementation "androidx.compose.material:material-icons-extended:1.6.0"
 
 }
---
+++ app/src/main/java/com/example/object_detection/CameraGalleryScreen.kt
@@ -0,0 +1,11 @@
+package com.example.object_detection
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraGalleryScreen(
+    onLiveDetectionClick: () -> Unit,
+    onGalleryClick:() -> Unit
+) {
+
+}
